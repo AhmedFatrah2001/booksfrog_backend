f13c631b6ea589ebc26f125d90e1f1edcf7fec98
hellodikki <dahbiayoub2001@gmail.com>
2024-12-23 17:35:31 +0100

    Add test to Main class

--- src/test/java/org/example/booksfrog/BooksfrogApplicationTests.java
+++ src/test/java/org/example/booksfrog/BooksfrogApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.booksfrog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BooksfrogApplication.class)
 class BooksfrogApplicationTests {
 
     @Test
