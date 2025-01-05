666ce8bf7700e9f1727a03214f93093c141e466a
AhmedFatrah2001 <fatrah.ahmed@gmail.com>
2025-01-05 20:47:41 +0100

    made the method to remove favorite book transactional so it works now ZzzZZZzzzZZ

--- src/main/java/org/example/booksfrog/service/FavoriteService.java
+++ src/main/java/org/example/booksfrog/service/FavoriteService.java
@@ -10,6 +10,7 @@ import org.example.booksfrog.repository.FavoriteRepository;
 import org.example.booksfrog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -63,7 +64,7 @@ public class FavoriteService {
                 .map(favorite -> new FavoriteIdDTO(favorite.getBook().getId()))
                 .collect(Collectors.toList());
     }
-
+    @Transactional
     public void removeFavorite(Long userId, Long bookId) {
         favoriteRepository.deleteByUserIdAndBookId(userId, bookId);
     }
